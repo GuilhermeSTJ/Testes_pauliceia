388ebbc9ff120b339200c692014da232393107a7
Guilherme Teodoro <teodoro538@hotmail.com>
2023-12-10 21:27:53 -0300

    Passos usando selenium para automatização de testes

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -1,21 +1,3 @@
-#Navigate to google.co.uk
-Given(/^I am on the Google homepage$/) do
-  visit 'https://www.google.com/'
-end
-
-#Write "yahoo" search text to the search bar
-When(/^I will search for "([^"]*)"$/) do |searchText|
-  fill_in 'APjFqb', :with => "Yahoo"
-end
-#In the current page, we should see "yahoo" text
-Then(/^I should see "([^"]*)"$/) do |expectedText|
-  page.should have_content(expectedText)
-end
-#Click the yahoo link
-Then(/^I will click the yahoo link$/) do
-  click_link('Yahoo')
-end
-#Wait 10 seconds statically to see yahoo website
 Then(/^I will wait for (\d+) seconds$/) do |waitTime|
   sleep (waitTime.to_i)
 end
